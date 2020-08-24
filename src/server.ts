5c45cb35c3d8d12b23466a4f5bc12d8387c54f8d
nhcarrigan <nhcarrigan@gmail.com>
2020-08-24 14:38:39 -0700

    No HTTPS

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected!"));
 
-app.use(helmet());
+app.use(helmet({ hsts: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(path.join(__dirname + "/../public")));
